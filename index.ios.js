5c537f5e4dd03e1594664c60f15917f26b669ba8
praneethy91 <praneethy91@gmail.com>
2017-02-18 06:11:09 -0500

    Fixing changes to the Hello world app

--- index.ios.js
+++ index.ios.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { AppRegistry, Text } from 'react-native';
 
-class HelloWorldApp extends Component {
+class flintWuberApp extends Component {
   render() {
     return (
       <Text>Hello world!</Text>
