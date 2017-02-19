118fcefb328e824b910aafb7f31584829afdc458
praneethy91 <praneethy91@gmail.com>
2017-02-19 01:30:40 -0500

    Adding markers to the map and showing them up. Static markers for now

--- index.android.js
+++ index.android.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Image, Text, View, Button, Alert, TextInput } from 'react-native';
+import { Dimensions, AppRegistry, StyleSheet, Image, Text, View, Button, Alert, TextInput } from 'react-native';
 import MapView from 'react-native-maps';
 
 const styles = StyleSheet.create({
@@ -15,7 +15,44 @@ const styles = StyleSheet.create({
  }
 });
 
+const { width, height } = Dimensions.get('window');
+
+const ASPECT_RATIO = width / height;
+
+const LATITUDE = 43.0125;
+const LONGITUDE = -83.6875;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const SPACE = 0.01;
+
 class flintWuberApp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      a: {
+        latitude: LATITUDE + SPACE,
+        longitude: LONGITUDE + SPACE,
+      },
+      b: {
+        latitude: LATITUDE - SPACE,
+        longitude: LONGITUDE - SPACE,
+      },
+      c: {
+        latitude: LATITUDE - (SPACE * 2),
+        longitude: LONGITUDE - (SPACE * 2),
+      },
+      d: {
+        latitude: LATITUDE - (SPACE * 3),
+        longitude: LONGITUDE - (SPACE * 3),
+      },
+      e: {
+        latitude: LATITUDE - (SPACE * 4),
+        longitude: LONGITUDE - (SPACE * 4),
+      },
+    };
+  }
+
   render() {
     const { region } = this.props;
     console.log(region);
@@ -27,12 +64,32 @@ class flintWuberApp extends Component {
         <MapView
           style={styles.container}
           region={{
-            latitude: 43.0125,
-            longitude: -83.6875,
+            latitude: LATITUDE,
+            longitude: LONGITUDE,
             latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
+            longitudeDelta: 0.0121
           }}
         >
+          <MapView.Marker
+            identifier="Marker1"
+            coordinate={this.state.a}
+          />
+          <MapView.Marker
+            identifier="Marker2"
+            coordinate={this.state.b}
+          />
+          <MapView.Marker
+            identifier="Marker3"
+            coordinate={this.state.c}
+          />
+          <MapView.Marker
+            identifier="Marker4"
+            coordinate={this.state.d}
+          />
+          <MapView.Marker
+            identifier="Marker5"
+            coordinate={this.state.e}
+          />
         </MapView>
         <View style={{marginTop: 540}}>
           <SubmitUserPreferenceButton />
@@ -88,7 +145,7 @@ class WaterTextInput extends Component {
         value={this.state.text}
         underlineColorAndroid="transparent"
       />
-    );
+    )
   }
 }
 
