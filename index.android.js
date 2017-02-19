de0b9fc0c1d421a3d47948c86cee998ef0e56997
praneethy91 <praneethy91@gmail.com>
2017-02-18 21:38:01 -0500

    Pusing changes to flint code

--- index.android.js
+++ index.android.js
@@ -19,23 +19,16 @@ class flintWuberApp extends Component {
   render() {
     const { region } = this.props;
     console.log(region);
-    let pic = {
-      uri: 'http://cdn2-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-8.jpg'
-    };
     return (
-      /*<View>
-        <Text>Hello World!</Text>
-        <Image source={pic} style={{ width: 193, height: 110 }} />
-      </View>*/
       <View style ={styles.container}>
-       <MapView
-         style={styles.map}
-         region={{
-           latitude: 37.78825,
-           longitude: -122.4324,
-           latitudeDelta: 0.015,
-           longitudeDelta: 0.0121,
-         }}
+        <MapView
+          style={styles.map}
+          region={{
+            latitude: 43.0125,
+            longitude: -83.6875,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
        >
        </MapView>
      </View>
